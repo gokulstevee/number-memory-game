50b4e7b06ce109309cf68a871b1b22c76fad82cc
gokulstevee <65470058+gokulstevee@users.noreply.github.com>
2020-08-05 03:54:42 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var resultArray
 var selectedElement
 var waitingforInput = false
+var oncepressed = false
 function generateRandomNumber(min, max) {
   var randomNumber = Math.floor(Math.random() * (max - min) + min).toString()
   resultArray = randomNumber.split("")
@@ -8,21 +9,22 @@ function generateRandomNumber(min, max) {
 
 generateRandomNumber(1111, 9999)
 
-
-
 function circleClicked(element, index) {
-  selectedElement = element
-  event.target.classList.add("wait")
-  waitingforInput = true
-}
+      selectedElement = element
+      event.target.classList.add("wait")
+      waitingforInput = true
+      oncepressed = true
+}  
+
+
 
 
 function detectKeyPress(event) {
   if (waitingforInput == true) {
+    if (event.keyCode>=48 && event.keyCode <=57)
       selectedElement.target.innerHTML = event.key
-
+      waitingforInput = false
+      selectedElement.target.classList.remove("wait")
   }
 
 }
-
-
